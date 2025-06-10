c43814398d44f180527b2daa880dbb192a03dd06
G1ANO <iwmuthiani@yahoo.com>
2025-06-11 02:11:03 +0300

    complete scope codelab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Write your solution in this file!
+var customerName = 'bob';
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+function setBestCustomer() {
+  bestCustomer = 'not bob'; // no `var`, `let`, or `const` used — creates global variable!
+}
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+const leastFavoriteCustomer = 'someone';
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'someone else';
+}
